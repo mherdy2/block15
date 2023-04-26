8f31e03fdf3e944e13e10458374a3fbcdbd7d037
Maui Bautista <mherdy_2@hotmail.com>
2023-04-25 20:05:15 -0700

    arrays

--- index.js
+++ index.js
@@ -11,27 +11,19 @@ const customer = {
   favoriteStore: "Target",
   firstVisit: false,
 };
-// console.table(customer);
-
 // gather the correct information. Modify the following properties in the customer object using bracket notation
-(customer["email"] = ["Jak3Smith1992@email.com"]),
-  (customer["phone"] = ["3195551234"]),
-  (customer["zipCode"] = ["63132"]),
+(customer["email"] = "Jak3Smith1992@email.com"),
+  (customer["phone"] = "3195551234"),
+  (customer["zipCode"] = "63132"),
   (customer["favoriteFlavors"] = ["coffee", "strawberry", "matcha"]);
-// console.table(customer);
-
 // remove a couple of items from the survey. Delete the following properties in the customer object using the delete keyword.
 delete customer["zipCode"];
 delete customer["favoriteStore"];
-// console.table(customer);
-
 // added the following items to the survey: toppings, future flavors, and today's purchase cost
 (customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"]),
   (customer.futureFlavors = ["mango"]),
   (customer.todaysPurchaseCost = 5.32);
-// console.table(customer);
-
 // In an array, print the keys in your survey
 for (const property in customer) {
-  console.table(property);
 }
+console.log(Object.keys(customer));
